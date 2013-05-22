9ac46befb9453aa0f5fc512eea7d8d745b894ff2
bootstraponline <code@bootstraponline.com>
2013-05-22 19:28:17 -0400

    Add button tests

--- lib/android/specs/common/element/button.rb
+++ lib/android/specs/common/element/button.rb
@@ -1,7 +1,49 @@
 # encoding: utf-8
-=begin
-describe 'test collection' do
-  t 'test one' do
+
+describe 'common/element/button' do
+  t 'button' do
+    # nav to buttons activity
+    text('App').click
+    text('Activity').click
+    text('Animation').click
+
+    # by index
+    button(0).name.must_equal 'Fade in'
+
+    # by name contains
+    button('ade').name.must_equal 'Fade in'
+  end
+
+  t 'buttons' do
+    exp = '[Fade in, Zoom in, Modern fade in, Modern zoom in, Scale up, Thumbnail zoom]'
+    buttons.to_s.must_equal exp
+  end
+
+  t 'first_button' do
+    first_button.name.must_equal 'Fade in'
+  end
+
+  t 'last_button' do
+    last_button.name.must_equal 'Thumbnail zoom'
+  end
+
+  t 'button_exact' do
+    button_exact('Fade in').name.must_equal 'Fade in'
+  end
+
+  t 'buttons_exact' do
+    buttons_exact('Fade in').first.name.must_equal 'Fade in'
+  end
+
+  t 'e_buttons' do
+    e_buttons.length.must_equal 6
+  end
+
+  t 'button_num' do
+    # select the second button containing 'in'
+    button_num('in', 2).name.must_equal 'Zoom in'
+
+    # nav back to home activity
+    3.times { back; sleep 0.5 }
   end
 end
-=end
