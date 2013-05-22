4e2722d6683e663cb74923307d4dd8b1c04b1781
bootstraponline <code@bootstraponline.com>
2013-05-22 18:21:43 -0400

    Add more tests
    Finished in 40 secs
    49 tests, 60 assertions

--- lib/android/specs/common/helper.rb
+++ lib/android/specs/common/helper.rb
@@ -57,73 +57,82 @@ describe 'common/helper.rb' do
     # start page
     text 'NFC'
   end
-=begin
-  t 'session_id' do
 
+  t 'session_id' do
+    session_id.must_match /\h{8}-\h{4}-\h{4}-\h{4}-\h{12}/
   end
 
   t 'xpath' do
-
+    xpath('//text').name.must_equal 'API Demos'
   end
 
   t 'xpaths' do
-
+    xpaths('//text').length.must_equal 12
   end
 
   t 'ele_index' do
-
+    ele_index('text', 1).name.must_equal 'Accessibility'
   end
 
   t 'find_eles' do
-
+    find_eles('text').length.must_equal 12
   end
 
   t 'find_ele_by_text' do
-
+    el_id = find_ele_by_text(:text, 'Accessibility').instance_variable_get :@id
+    el_id.must_match /\d+/
   end
 
   t 'find_eles_by_text' do
+    # '!' clears the input buffer in Pry so make sure there's
+    # no space after the !
+    # empty array returned when no match
+    found = !find_eles_by_text(:text, 'App').empty?
+    found.must_equal true
 
+    found = !find_eles_by_text(:text, 'zz').empty?
+    found.must_equal false
   end
 
   t 'find_ele_by_attr_include' do
-
+    el_text = find_ele_by_attr_include(:text, :text,  'acc').text
+    el_text.must_equal 'Accessibility'
   end
 
   t 'find_eles_by_attr_include' do
-
+    ele_count = find_eles_by_attr_include(:text, :text,  'e').length
+    ele_count.must_equal 7
   end
 
   t 'find_ele_by_text_include' do
-
+    el_text = find_ele_by_text_include(:text, 'acc').text
+    el_text.must_equal 'Accessibility'
   end
 
   t 'find_eles_by_text_include' do
-
+    ele_count = find_eles_by_text_include(:text, 'e').length
+    ele_count.must_equal 7
   end
 
   t 'first_ele' do
-
+    first_ele(:text).text.must_equal 'API Demos'
   end
 
   t 'last_ele' do
-
+    last_ele(:text).text.must_equal 'Views'
   end
 
-  t 'source' do
-
-  end
+  # t 'source' do # tested by get_source
 
   t 'get_source' do
-
+    get_source.class.must_equal Hash
   end
 
   t 'find_name' do
-
+    find_name('App').name.must_equal 'App'
   end
 
   t 'find_names' do
-
+    find_names('App').first.name.must_equal 'App'
   end
-=end
 end
