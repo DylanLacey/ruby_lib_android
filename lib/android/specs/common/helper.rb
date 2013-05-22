2185a5fb82cfbfdc916ed3780c6ff8a0ea7a6585
bootstraponline <code@bootstraponline.com>
2013-05-22 16:19:44 -0400

    Add wait tests

--- lib/android/specs/common/helper.rb
+++ lib/android/specs/common/helper.rb
@@ -1,7 +1,118 @@
 # encoding: utf-8
-=begin
-describe 'test collection' do
-  t 'test one' do
+describe 'common/helper.rb' do
+  # wait is a success unless an error is raised
+  # max_wait=0 is infinity to use 0.1
+  t 'wait' do
+    # successful wait should not error
+    message = nil
+    begin
+      wait(0.1,0) { true }
+    rescue Exception => e
+      message = e.message
+    end
+    message.must_equal nil
+
+    # failed wait should error
+    begin
+      wait(0.1,0) { raise 'error' }
+    rescue Exception => e
+      message = e.message
+    end
+    message.must_equal 'execution expired'
   end
-end
+
+  # wait_true is a success unless the value is not true
+  t 'wait_true' do
+    # successful wait should not error
+    message = nil
+    begin
+      wait_true(0.1,0) { true }
+    rescue Exception => e
+      message = e.message
+    end
+    message.must_equal nil
+
+    # failed wait should error
+    begin
+      wait_true(0.1,0) { false }
+    rescue Exception => e
+      message = e.message
+    end
+    message.must_equal 'execution expired'
+  end
+
+  # t 'id' # id is for Selendroid
+=begin
+  t 'back' do
+
+  end
+
+  t 'session_id' do
+
+  end
+
+  t 'xpath' do
+
+  end
+
+  t 'xpaths' do
+
+  end
+
+  t 'ele_index' do
+
+  end
+
+  t 'find_eles' do
+
+  end
+
+  t 'find_ele_by_text' do
+
+  end
+
+  t 'find_eles_by_text' do
+
+  end
+
+  t 'find_ele_by_attr_include' do
+
+  end
+
+  t 'find_eles_by_attr_include' do
+
+  end
+
+  t 'find_ele_by_text_include' do
+
+  end
+
+  t 'find_eles_by_text_include' do
+
+  end
+
+  t 'first_ele' do
+
+  end
+
+  t 'last_ele' do
+
+  end
+
+  t 'source' do
+
+  end
+
+  t 'get_source' do
+
+  end
+
+  t 'find_name' do
+
+  end
+
+  t 'find_names' do
+
+  end
 =end
+end
