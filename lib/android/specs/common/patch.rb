c8cbfdd47d418ed55cb02afd600822d59972609e
bootstraponline <code@bootstraponline.com>
2013-05-22 16:00:33 -0400

    Add name test

--- lib/android/specs/common/patch.rb
+++ lib/android/specs/common/patch.rb
@@ -6,26 +6,23 @@
     raw_execute # debug output for Pry
 =end
 
-=begin
 # Attributes are busted in Android.
 # Blocked on https://github.com/appium/appium/issues/628
 describe 'Selenium::WebDriver::Element methods' do
-  element = s_first_text
-  t 'value' do
-
-  end
+  # Android supports exactly two string Attributes
+  # .name and .text
+  # https://github.com/appium/appium/blob/ea3450e7f78d1794bab42fa396a387e7b86fd3b3/android/bootstrap/src/io/appium/android/bootstrap/handler/GetAttribute.java#L43
+  # t 'value' do; end # Doesn't work on Android
 
   t 'name' do
-
+    first_s_text.name.must_equal 'API Demos'
   end
 
-  t 'tag_name' do
-
-  end
+  # t 'tag_name' do; end # Doesn't work on Android
 
   # location_rel # iOS Only. Android is blocked on https://github.com/appium/appium/issues/627
 end
-=end
+
 
 describe 'common patch' do
   # By default, the webdriver gem will return message instead of origValue
